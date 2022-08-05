3a3b2eb8602003414f9b2bac37ad4d3bb9bb5aec
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-08-05 19:46:37 +0600

    feat(server): add todo crud features

---
+++ src/server/router/todo.ts
@@ -0,0 +1,53 @@
+import { z } from "zod";
+import { createRouter } from "./context";
+
+export const todoRouter = createRouter()
+  .query("all", {
+    async resolve({ ctx }) {
+      return ctx.task.findMany({
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    },
+  })
+  .mutation("add", {
+    input: z.object({
+      content: z.string().min(2),
+      userId: z.string().optional(),
+    }),
+    async resolve({ ctx, input }) {
+      const todo = await ctx.task.create({ data: input });
+      return todo;
+    },
+  })
+  .mutation("edit", {
+    input: z.object({
+      id: z.string().cuid(),
+      data: z.object({
+        isDone: z.boolean().optional(),
+        content: z.string().min(1).optional(),
+      }),
+    }),
+    async resolve({ ctx, input }) {
+      const { id, data } = input;
+      const todo = await ctx.task.update({
+        where: { id },
+        data,
+      });
+      return todo;
+    },
+  })
+  .mutation("delete", {
+    input: z.string().cuid(),
+    async resolve({ input: id, ctx }) {
+      await ctx.task.delete({ where: { id } });
+      return id;
+    },
+  })
+  .mutation("clearCompleted", {
+    async resolve({ ctx }) {
+      await ctx.task.deleteMany({ where: { isDone: true } });
+      return ctx.task.findMany();
+    },
+  });
--- src/server/router/user.ts
+++ src/server/router/user.ts
@@ -2,11 +2,8 @@ import { createRouter } from "./context";
 
 export const userRouter = createRouter().query("hi", {
   async resolve({ ctx }) {
-    const user = await ctx.prisma.user.findFirst({
-      where: { id: ctx.session?.user?.id },
-    });
     return {
-      greeting: `Hi, ${user?.name}!`,
+      greeting: `Hi, ${ctx.session?.user?.name}!`,
     };
   },
 });
